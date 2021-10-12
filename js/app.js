54e8ec2090bcbb2b203865ca8ab120ded5f0f5cf
SyedAli310 <alizaidi310@gmail.com>
2021-10-12 19:34:47 +0530

    bug fix (hamburger button)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $('.hamburger').on('click',()=>{
     $('.navbar').toggleClass('responsive')
     $('.nav-links').toggleClass('responsive')
+    $('.hamburger').toggleClass('responsive')
     console.log($('.hamburger').attr('name'))
 })
