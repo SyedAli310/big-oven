f5b2d300e965f8153f38dbae4eb6619aa47aa15e
SyedAli310 <alizaidi310@gmail.com>
2021-10-12 20:34:41 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 let x = null
-$('.hamburger').on('click',()=>{
+$('.hamburger').on('click',(e)=>{
     $('.navbar').toggleClass('responsive')
     $('.hamburger').toggleClass('responsive')
     $('.nav-links').toggleClass('responsive')
- 
+    if(e.target.name == 'menu-outline'){
+        e.target.name = 'close-outline'
+    }
+    else if(e.target.name=='close-outline'){
+        e.target.name = 'menu-outline'
+    }
 })
